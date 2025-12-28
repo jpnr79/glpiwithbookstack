238bfc23e2b56c782b633551ea4acc123b29f028
jpnr79 <jpnr79@gmail.com>
2025-12-28 23:07:24 +0000

    Implement getConfigurationValues method to retrieve plugin configuration from the database

--- inc/config.class.php
+++ inc/config.class.php
@@ -1,3 +1,22 @@
+   /**
+    * Return plugin configuration as associative array
+    * @param string $context (e.g. 'plugin:Glpiwithbookstack')
+    * @return array
+    */
+   public static function getConfigurationValues($context = 'plugin:Glpiwithbookstack') {
+      global $DB;
+      $config = [];
+      $res = $DB->request([
+         'FROM' => 'glpi_plugin_glpiwithbookstack_configs',
+         'WHERE' => []
+      ]);
+      if ($res && count($res)) {
+         foreach ($res as $row) {
+            $config[$row['name']] = $row['value'];
+         }
+      }
+      return $config;
+   }
 <?php
 if (!defined('GLPI_ROOT')) { define('GLPI_ROOT', realpath(__DIR__ . '/../..')); }
 
